a3ae4e06afe95671031310b19967cc835578802e
Alec Xu <alec_xu@hotmail.com>
2018-09-05 14:37:53 -0700

    Create initial layout for X Y DeltaT and drawing window

--- Project 1/Project 1/Form1.cs
+++ Project 1/Project 1/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_1
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
