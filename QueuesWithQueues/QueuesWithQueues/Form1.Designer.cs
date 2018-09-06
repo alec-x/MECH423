14da20794c271b44c2abcbf15cd4cd39abbad3bd
Alec Xu <alec_xu@hotmail.com>
2018-09-06 10:48:05 -0700

    finish implementing name for elements and implementing anchoring

--- QueuesWithQueues/QueuesWithQueues/Form1.Designer.cs
+++ QueuesWithQueues/QueuesWithQueues/Form1.Designer.cs
@@ -28,73 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.ButtonAddToQueue = new System.Windows.Forms.Button();
+            this.ButtonGetFromQueue = new System.Windows.Forms.Button();
+            this.ButtonQueueItems = new System.Windows.Forms.Button();
+            this.ButtonAvg = new System.Windows.Forms.Button();
+            this.TextAddToQueue = new System.Windows.Forms.TextBox();
+            this.TextGetFromQueue = new System.Windows.Forms.TextBox();
+            this.TextN = new System.Windows.Forms.TextBox();
+            this.TextQueueItems = new System.Windows.Forms.TextBox();
+            this.TextAvg = new System.Windows.Forms.TextBox();
+            this.LabelN = new System.Windows.Forms.Label();
+            this.LabelAvg = new System.Windows.Forms.Label();
+            this.ListQueueContents = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // ButtonAddToQueue
             // 
-            this.button1.Font = new System.Drawing.Font("Consolas", 11.8F);
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add to Queue";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ButtonAddToQueue.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.ButtonAddToQueue.Location = new System.Drawing.Point(12, 12);
+            this.ButtonAddToQueue.Name = "ButtonAddToQueue";
+            this.ButtonAddToQueue.Size = new System.Drawing.Size(172, 30);
+            this.ButtonAddToQueue.TabIndex = 0;
+            this.ButtonAddToQueue.Text = "Add to Queue";
+            this.ButtonAddToQueue.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // ButtonGetFromQueue
             // 
-            this.button2.Font = new System.Drawing.Font("Consolas", 11.8F);
-            this.button2.Location = new System.Drawing.Point(12, 48);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 30);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Get from Queue";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ButtonGetFromQueue.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.ButtonGetFromQueue.Location = new System.Drawing.Point(12, 51);
+            this.ButtonGetFromQueue.Name = "ButtonGetFromQueue";
+            this.ButtonGetFromQueue.Size = new System.Drawing.Size(172, 30);
+            this.ButtonGetFromQueue.TabIndex = 1;
+            this.ButtonGetFromQueue.Text = "Get from Queue";
+            this.ButtonGetFromQueue.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // ButtonQueueItems
             // 
-            this.button3.Font = new System.Drawing.Font("Consolas", 11.8F);
-            this.button3.Location = new System.Drawing.Point(12, 84);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(172, 30);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Items in Queue";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ButtonQueueItems.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.ButtonQueueItems.Location = new System.Drawing.Point(12, 90);
+            this.ButtonQueueItems.Name = "ButtonQueueItems";
+            this.ButtonQueueItems.Size = new System.Drawing.Size(172, 30);
+            this.ButtonQueueItems.TabIndex = 2;
+            this.ButtonQueueItems.Text = "Items in Queue";
+            this.ButtonQueueItems.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // ButtonAvg
             // 
-            this.button4.Font = new System.Drawing.Font("Consolas", 11.8F);
-            this.button4.Location = new System.Drawing.Point(12, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(172, 30);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Avg of Last N";
-            this.button4.UseVisualStyleBackColor = true;
+            this.ButtonAvg.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.ButtonAvg.Location = new System.Drawing.Point(12, 129);
+            this.ButtonAvg.Name = "ButtonAvg";
+            this.ButtonAvg.Size = new System.Drawing.Size(172, 30);
+            this.ButtonAvg.TabIndex = 3;
+            this.ButtonAvg.Text = "Avg of Last N";
+            this.ButtonAvg.UseVisualStyleBackColor = true;
+            // 
+            // TextAddToQueue
+            // 
+            this.TextAddToQueue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextAddToQueue.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.TextAddToQueue.Location = new System.Drawing.Point(190, 11);
+            this.TextAddToQueue.Name = "TextAddToQueue";
+            this.TextAddToQueue.Size = new System.Drawing.Size(308, 31);
+            this.TextAddToQueue.TabIndex = 5;
+            // 
+            // TextGetFromQueue
+            // 
+            this.TextGetFromQueue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextGetFromQueue.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.TextGetFromQueue.Location = new System.Drawing.Point(190, 50);
+            this.TextGetFromQueue.Name = "TextGetFromQueue";
+            this.TextGetFromQueue.Size = new System.Drawing.Size(308, 31);
+            this.TextGetFromQueue.TabIndex = 6;
+            // 
+            // TextN
+            // 
+            this.TextN.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.TextN.Location = new System.Drawing.Point(221, 129);
+            this.TextN.Name = "TextN";
+            this.TextN.Size = new System.Drawing.Size(83, 31);
+            this.TextN.TabIndex = 7;
+            // 
+            // TextQueueItems
+            // 
+            this.TextQueueItems.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextQueueItems.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.TextQueueItems.Location = new System.Drawing.Point(190, 89);
+            this.TextQueueItems.Name = "TextQueueItems";
+            this.TextQueueItems.Size = new System.Drawing.Size(308, 31);
+            this.TextQueueItems.TabIndex = 8;
+            // 
+            // TextAvg
+            // 
+            this.TextAvg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextAvg.Font = new System.Drawing.Font("Consolas", 11.8F);
+            this.TextAvg.Location = new System.Drawing.Point(367, 129);
+            this.TextAvg.Name = "TextAvg";
+            this.TextAvg.Size = new System.Drawing.Size(131, 31);
+            this.TextAvg.TabIndex = 9;
+            // 
+            // LabelN
+            // 
+            this.LabelN.AutoSize = true;
+            this.LabelN.Font = new System.Drawing.Font("Consolas", 13.8F);
+            this.LabelN.Location = new System.Drawing.Point(190, 130);
+            this.LabelN.Name = "LabelN";
+            this.LabelN.Size = new System.Drawing.Size(25, 28);
+            this.LabelN.TabIndex = 10;
+            this.LabelN.Text = "N";
+            // 
+            // LabelAvg
+            // 
+            this.LabelAvg.AutoSize = true;
+            this.LabelAvg.Font = new System.Drawing.Font("Consolas", 13.8F);
+            this.LabelAvg.Location = new System.Drawing.Point(310, 130);
+            this.LabelAvg.Name = "LabelAvg";
+            this.LabelAvg.Size = new System.Drawing.Size(51, 28);
+            this.LabelAvg.TabIndex = 11;
+            this.LabelAvg.Text = "Avg";
+            // 
+            // ListQueueContents
+            // 
+            this.ListQueueContents.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListQueueContents.FormattingEnabled = true;
+            this.ListQueueContents.ItemHeight = 16;
+            this.ListQueueContents.Location = new System.Drawing.Point(12, 165);
+            this.ListQueueContents.Name = "ListQueueContents";
+            this.ListQueueContents.Size = new System.Drawing.Size(486, 228);
+            this.ListQueueContents.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(511, 403);
+            this.Controls.Add(this.ListQueueContents);
+            this.Controls.Add(this.LabelAvg);
+            this.Controls.Add(this.LabelN);
+            this.Controls.Add(this.TextAvg);
+            this.Controls.Add(this.TextQueueItems);
+            this.Controls.Add(this.TextN);
+            this.Controls.Add(this.TextGetFromQueue);
+            this.Controls.Add(this.TextAddToQueue);
+            this.Controls.Add(this.ButtonAvg);
+            this.Controls.Add(this.ButtonQueueItems);
+            this.Controls.Add(this.ButtonGetFromQueue);
+            this.Controls.Add(this.ButtonAddToQueue);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button ButtonAddToQueue;
+        private System.Windows.Forms.Button ButtonGetFromQueue;
+        private System.Windows.Forms.Button ButtonQueueItems;
+        private System.Windows.Forms.Button ButtonAvg;
+        private System.Windows.Forms.TextBox TextAddToQueue;
+        private System.Windows.Forms.TextBox TextGetFromQueue;
+        private System.Windows.Forms.TextBox TextN;
+        private System.Windows.Forms.TextBox TextAvg;
+        private System.Windows.Forms.Label LabelN;
+        private System.Windows.Forms.Label LabelAvg;
+        private System.Windows.Forms.ListBox ListQueueContents;
+        private System.Windows.Forms.TextBox TextQueueItems;
     }
 }
 
