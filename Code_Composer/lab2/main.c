d4f3cb3887776c37a2450e0da7896ac984615069
Alec Xu <alec_xu@hotmail.com>
2018-09-21 17:38:14 -0700

    finished exercise 1 lab2 untested

---
+++ Code_Composer/lab2/main.c
@@ -0,0 +1,20 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    // |= REG_NAME sets the bit at that bit. REG_NAME + REG_NAME_2 sets the bit at those 2 bits.
+
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	CSCTL2 |= SELS_1 + SELS_2; //set SMCLK to DCO, 011b
+	CSCTL1 &= ~DCORSEL; // make sure high freq select is off
+	CSCTL1 |= DCOFSEL_0 + DCOFSEL_1; // set DCO to 8MHz, 11b
+	CSCTL3 |= DIVS_0 + DIVS_2; // set SMCLK divisor to 32, 101b
+	P3DIR |= BIT0; //Set P3 direction to output, 1b
+
+
+	return 0;
+}
