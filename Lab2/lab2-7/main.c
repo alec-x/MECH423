a1923c858c134d37d7a28f0c45a0e785b4d8956e
Alec Xu <alec_xu@hotmail.com>
2018-10-09 10:44:03 -0700

    finished lab 2-7

--- Lab2/lab2-7/main.c
+++ Lab2/lab2-7/main.c
@@ -1,50 +1,87 @@
 #include <msp430.h> 
 
+volatile unsigned char xAccel = 0;
+volatile unsigned char yAccel = 0;
+volatile unsigned char zAccel = 0;
 
-/**
- * main.c
- */
 int main(void)
 {
+    int counter; // x = 0, y = 1, z = 2
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	// set dco clock to 1MHz
-    CSCTL1 |= DCOFSEL0 + DCOFSEL1;       // Set max. DCO setting =8MHz
-    CSCTL2 = SELA_3 + SELS_3 + SELM_3;   // set ACLK = SMCLK = MCLK = DCO/8
-    CSCTL3 = DIVA_3 + DIVS_3 + DIVM_3;   // set all dividers
 
+	// Power accelerometer
 	P2DIR |= BIT7; // configure P2 to be output
+	P2OUT |= BIT7; // configure P2 output to be high
 	
+	// set DCO clock to 1MHz, assign to all clocks
+	CSCTL0_H = 0xA5;
+    CSCTL1 |= DCOFSEL0 + DCOFSEL1;       // Set max. DCO setting =8MHz
+    CSCTL2 = SELA_3 + SELS_3 + SELM_3;   // set ACLK = SMCLK = MCLK
+
 	//Configure timer interrupt
-    P3DIR |= BIT4;                       // P3.4 output
-    P3SEL0 |= BIT4;                      // P3.4 options select
-    TB1CCR0 = 2000;                      // PWM Period clock = 1MHz, desired freq 500Hz, factor 2000
-    TB1CCTL1 = OUTMOD_7;                 // CCR1 reset/set
-    TB1CCR1 = 500;                      // CCR1 PWM duty cycle
-    TB1CTL = TBSSEL_1 + MC_1;            //set up  timer B in up count mode with ACLK as source
+    TA0CTL = TASSEL_1 + MC_1 + ID_3;      // use ACLKC
+    TA0CCTL0 = CCIE;               // count to TA0CCR0, enable interrupt
+    TA0CCR0 = 40000;               // PWM Period clock = 8MHz, desired freq 25Hz, factor 40000 * 8 (in TA0CTL)
 
-	// sample from PA12,13,14 = P2.4-2.6
-	ADC10CTL0 &= ~ADC10ENC; //settings can only be changed when this is true
-    ADC10CTL0 = ADC10SHT_0 + ADC10ON;// 16ADCclks, ADC ON
-    ADC10CTL0 &= ~ADC10MSC; //make sure MSC is off so we get chance to record data in vars without direct memory access
-    ADC10CTL1 = ADC10CONSEQ_1;     // multiple channel single sequence
-    ADC10CTL2 &= ~ADC10RES;                   // 8-bit resolution
-    ADC10MCTL0 = ADC10_INCH_14; // sample from 14 -> 0, must reset ADC10ENC at x = 12
+    //Configure A12-14 as Analog in
+    P3SEL0 |= BIT0 + BIT1 + BIT2;
+    P3SEL1 |= BIT0 + BIT1 + BIT2;
 
+    //Configure UART
+    P2SEL0 &= ~(BIT0 + BIT1); //Configure ports to be UART
+    P2SEL1 |= BIT0 + BIT1;
+    UCA0CTLW0 = UCSSEL0; // clock for UART comes from ACLK, also UART is enabled
+    UCA0MCTLW = 0x4900 + UCOS16 + UCBRF0;
+    UCA0BRW = 52;
+	
+	//Configure ADC
+    ADC10CTL0 = ADC10SHT_2 + ADC10ON;                         // 4ADCclks, ADC ON
+    ADC10CTL1 = ADC10CONSEQ_0 + ADC10SHP + ADC10SSEL_1;       // multiple channel single sequence
+    ADC10CTL2 |= ADC10RES;                                    // 10-bit resolution, shift when read
+    ADC10MCTL0 |= ADC10INCH_12; //start with channel 12, x
+    counter = 0; // x = 0, y = 1, z = 2
     _EINT(); //global interrupt enable
+
+
     while(1)
     {
+        while (ADC10CTL1 & BUSY);               // Wait if ADC10 core is active
+        ADC10CTL0 |= ADC10ENC + ADC10SC;        // Start sampling
 
-      while (ADC10CTL1 & BUSY);               // Wait if ADC10 core is active
-      ADC10CTL0 |= ADC10ENC + ADC10SC;        // Sampling and conversion start
-      __no_operation();                       // BREAKPOINT; check ADC_Result
+        if(counter == 0){
+             xAccel = ADC10MEM0 >> 2;
+             ADC10CTL0 &= ~(ADC10ENC + ADC10SC);
+             ADC10MCTL0 = ADC10INCH_13;
+             counter++;
+         }
+         else if(counter == 1){
+             yAccel = ADC10MEM0 >> 2;
+             ADC10CTL0 &= ~(ADC10ENC + ADC10SC);
+             ADC10MCTL0 = ADC10INCH_14;
+             counter++;
+         }
+         else if(counter == 2){
+             zAccel = ADC10MEM0 >> 2;
+             ADC10CTL0 &= ~(ADC10ENC + ADC10SC);
+             ADC10MCTL0 = ADC10INCH_12;
+             counter = 0;
+         }
     }
-
-
-	return 0;
 }
 
-#pragma vector = TIMER0_A1_VECTOR
+#pragma vector = TIMER0_A0_VECTOR
 __interrupt void TIMER0_ISR(void)
 {
-}
+    while(!(UCA0IFG & UCTXIFG)); //if currently transmitting, then loop on itself
+    UCA0TXBUF = 255;
+
+    while(!(UCA0IFG & UCTXIFG)); //if currently transmitting, then loop on itself
+    UCA0TXBUF = xAccel;
+
+    while(!(UCA0IFG & UCTXIFG)); //if currently transmitting, then loop on itself
+    UCA0TXBUF = yAccel;
+
+    while(!(UCA0IFG & UCTXIFG)); //if currently transmitting, then loop on itself
+    UCA0TXBUF = zAccel;
+
 }
--- Lab2/lab2-8/main.c
+++ Lab2/lab2-8/main.c
@@ -1,19 +1,93 @@
-#include <msp430.h>				
+#include <msp430.h>
 
+volatile unsigned char x;
+volatile unsigned char y;
+volatile unsigned char z;
 
 /**
- * blink.c
+ * main.c
  */
-void main(void)
+int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
+    WDTCTL = WDTPW | WDTHOLD;   // Stop WD Timer
 
-	volatile unsigned int i;		// volatile to prevent optimization
+	// Power accelerometer
+    P2DIR |= BIT7; // configure P2 to be output
+    P2OUT |= BIT7; // configure P2 output to be high
 
-	while(1)
-	{
-		P1OUT ^= 0x01;				// toggle P1.0
-		for(i=10000; i>0; i--);     // delay
-	}
+	// set DCO clock to 1MHz, assign to all clocks
+    CSCTL0_H = 0xA5;                                            // Unlock Clocks
+    CSCTL1 = DCOFSEL0 + DCOFSEL1;                               // Set DCO to 8 MHz
+    CSCTL2 = SELM__DCOCLK + SELA__DCOCLK + SELS__DCOCLK;        // Set all clocks to src DCO
+	
+    TB1CCR0 = 5000 - 1;
+    TB1CCTL1 = CCIE;
+    TB1CTL = TBSSEL__SMCLK + MC__UP + ID__8;
+    TB0EX0 = TBIDEX__8;
+    TB1CTL |= TBCLR;
+
+	//Configure A12-14 as Analog in
+	P3SEL0 |= BIT0 + BIT1 + BIT2; // X Y Z
+    P3SEL1 |= BIT0 + BIT1 + BIT2;
+	
+	//Configure UART
+    P2SEL0 &= ~(BIT0 + BIT1);                                   //Config. UART Port
+    P2SEL1 |= BIT0 + BIT1;
+    UCA0CTLW0 = UCSSEL0;                                        // ACLK src, also UART is enabled
+    UCA0MCTLW = 0x4900 + UCOS16 + UCBRF0;                       // Table 18-5 Data Sheet, p. 491
+    UCA0BRW = 52;
+
+	//Configure ADC
+    ADC10CTL0 |= ADC10ON;                   // Turn on
+    ADC10CTL1 |= ADC10SHP  + ADC10CONSEQ_0; // From sampling timer
+    ADC10CTL2 |= ADC10RES;                  // Set incoming res. to 10 bits
+    _EINT();
+
+    while(1){
+
+        // x
+        ADC10CTL0 &= ~(ADC10ENC + ADC10SC); // Disable conversion to set new channel
+        ADC10MCTL0 = ADC10INCH_12;         // Turn on x-channel (A12)
+        ADC10CTL0 |= ADC10ENC + ADC10SC;    // Enable and start
+        while(ADC10CTL1 & ADC10BUSY);
+
+        x = ADC10MEM0 >> 2;                 // Bitshift right by 2
+        _NOP();
+
+
+        // y
+        ADC10CTL0 &= ~(ADC10ENC + ADC10SC); // Disable conversion to set new channel
+        ADC10MCTL0 = ADC10INCH_13;         // Turn on y-channel (A13)
+        ADC10CTL0 |= ADC10ENC + ADC10SC;    // Enable and start
+        while(ADC10CTL1 & ADC10BUSY);
+
+        y = ADC10MEM0 >> 2;                 // Bitshift right by 2
+        _NOP();
+
+
+        // z
+        ADC10CTL0 &= ~(ADC10ENC + ADC10SC); // Disable conversion to set new channel
+        ADC10MCTL0 = ADC10INCH_14;         // Turn on z-channel (A14)
+        ADC10CTL0 |= ADC10ENC + ADC10SC;    // Enable and start
+        while(ADC10CTL1 & ADC10BUSY);
+
+        z = ADC10MEM0 >> 2;                 // Bitshift right by 2
+        _NOP();
+    }
+
+    return 0;
+}
+
+#pragma vector = TIMER1_B1_VECTOR
+__interrupt void TIMER1_B1_ISR(void){
+    while(!(UCA0IFG & UCTXIFG));
+    UCA0TXBUF = 255;
+    while(!(UCA0IFG & UCTXIFG));
+    UCA0TXBUF = x;
+    while(!(UCA0IFG & UCTXIFG));
+    UCA0TXBUF = y;
+    while(!(UCA0IFG & UCTXIFG));
+    UCA0TXBUF = z;
+    _NOP();
+    TB1CCTL1 ^= CCIFG;
 }
