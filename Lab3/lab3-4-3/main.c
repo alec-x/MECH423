b5035d9689d47cc78a96f392f5d0922e3318e3c5
Alec Xu <alec_xu@hotmail.com>
2018-11-03 16:12:45 -0700

    implemented motor control

--- Lab3/lab3-4-3/main.c
+++ Lab3/lab3-4-3/main.c
@@ -6,6 +6,15 @@ volatile unsigned char up1 = 0;
 volatile unsigned char up0 = 0;
 volatile unsigned char down1 = 0;
 volatile unsigned char down0 = 0;
+
+const int queueSize = 50;
+volatile unsigned int currSize = 0;
+volatile unsigned int last = 0;
+volatile unsigned int curr = 0;
+char queue[50];
+unsigned int freqInt;
+char dataByte1, dataByte2, directionByte, escapeByte2;
+
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
@@ -36,20 +45,110 @@ int main(void)
 
 
     //Configure timer interrupt
-    TB0CTL = TBSSEL_1 + MC_1 + ID_3;      // use ACLKC
-    TB0CCTL0 = CCIE;               // count to TA0CCR0, enable interrupt
-    TB0CCR0 = 50000;               // PWM Period clock = 8MHz, desired freq 25Hz, factor 40000 * 8 (in TA0CTL)
+    TB1CTL = TBSSEL_1 + MC_1 + ID_3;      // use ACLKC
+    TB1CCTL0 = CCIE;               // count to TA0CCR0, enable interrupt
+    TB1CCR0 = 50000;               // PWM Period clock = 8MHz, desired freq 25Hz, factor 40000 * 8 (in TA0CTL)
 
+    //Configure Duty cycle (A1 and A2)
+    P1DIR |= BIT4 + BIT5;                       // P1.4, 1.5 output
+    P1SEL0 |= BIT4 + BIT5;                      // P1.4, 1.5 options select
+    TB0CCTL1 = OUTMOD_7;                 // CCR1 reset/set
+    TB0CCTL2 = OUTMOD_7;                 // CCR2 reset/set
+    TB0CCR1 = 15000;                      // CCR1 PWM duty cycle, PWM Period clock = 8MHz
+    TB0CCR2 = 100;                      // CCR1 PWM duty cycle, PWM Period clock = 8MHz
+    TB0CTL = TBSSEL_1 + MC_2;            //set up  timer B in up count mode with ACLK as source
 
 
     _EINT(); //global interrupt enable
 
 
-    while(1);
+    while(1){
+        while(queue[last] != 255 && currSize > 0){
+            queue[last] = 0;
+            currSize -= 1;
+
+            if(last == 49){
+                last = 0;
+            }
+            else{
+                last += 1;
+            }
+        }
+        while(currSize < 5);
+        queue[last] = 0;
+        currSize -= 1;
+        if(last == 49){
+            last = 0;
+        }
+        else{
+            last += 1;
+        }
+
+        dataByte1 = queue[last];
+        queue[last] = 0;
+        currSize -= 1;
+        if(last == 49){
+            last = 0;
+        }
+        else{
+            last += 1;
+        }
+
+        dataByte2 = queue[last];
+        queue[last] = 0;
+        currSize -= 1;
+        if(last == 49){
+            last = 0;
+        }
+        else{
+            last += 1;
+        }
+
+        directionByte = queue[last];
+        queue[last] = 0;
+        currSize -= 1;
+        if(last == 49){
+            last = 0;
+        }
+        else{
+            last += 1;
+        }
+
+        escapeByte2 = queue[last];
+        queue[last] = 0;
+        currSize -= 1;
+        if(last == 49){
+            last = 0;
+        }
+        else{
+            last += 1;
+        }
+
+        if(escapeByte2 == 1){
+            dataByte1 = 255;
+        } else if(escapeByte == 2){
+            dataByte2 = 255;
+        } else if(escapeByte ==3){
+            dataByte1 = 255;
+            dataByte2 = 255;
+        }
+
+        freqInt = (dataByte1 << 8) + dataByte2;
+        TB0CCR2 = freqInt;
+        TB0CCR1 = freqInt;
+
+        if(directionByte){
+            TB0CCR1 = 0;
+        }
+        else{
+            TB0CCR2 = 0;
+        }
+
+    }
 }
 
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void TIMER0_ISR(void)
+#pragma vector = TIMER1_B0_VECTOR
+__interrupt void TIMER1_ISR(void)
 {
     // 255, UP1, UP0, DOWN1, DOWN0, ESCAPE
     // where 1 is most sig, 0 is least
@@ -91,4 +190,23 @@ __interrupt void TIMER0_ISR(void)
 
     TA0R = 0;
     TA1R = 0;
+}
+
+#pragma vector = USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+{
+    unsigned char RxByte = 0;
+    RxByte = UCA1RXBUF;
+
+    if(currSize < queueSize){
+        queue[curr] = RxByte;
+        if(curr == 49){
+            curr = 0;
+        }else{
+            curr++;
+        }
+        currSize += 1;
+    }
+
+    __no_operation();
 }
